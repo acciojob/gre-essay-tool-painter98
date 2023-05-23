d52294704603d988355cf410224932881960526d
Pallavi <120320713+painter98@users.noreply.github.com>
2023-05-23 12:07:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+let count = 0;
+let word = document.getElementById('evaluatedText');
+let wordCount = document.getElementById('wordCount');
+
+word.addEventListener('keyup',(event)=>{
+	if(event.code == 'Space') {
+		count++;
+		wordCount.innerText = count;
+	}
+})
