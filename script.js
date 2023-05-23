8960decf80d0a2361eac2f95e8d36f7d331c70db
Pallavi <120320713+painter98@users.noreply.github.com>
2023-05-23 12:17:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//your code here
 let count = 0;
 let word = document.getElementById('evaluatedText');
 let wordCount = document.getElementById('wordCount');
+word.innerText = count;
 
-word.addEventListener('keyup',(event)=>{
-	if(event.code == 'Space') {
-		count++;
-		wordCount.innerText = count;
-	}
+word.addEventListener('input',(event)=>{
+	let str = event.target.value.trim().split(' ');
+	count = str.length;
+	wordCount.innerText = count;
+	
 })
